12baa4417428585391354e4043c9476b1d9892e1
Romashka8 <seleznyov.romario@mail.ru>
2026-05-23 12:59:31 +0300

    🟨feat: added regularization - dropout and batch normalization

--- MLP(week-1)/simple_autograd.py
+++ MLP(week-1)/simple_autograd.py
@@ -306,3 +306,142 @@ class Sequential(Layer):
 
 
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Регуляризация: Dropout + BatchNorm
+
+
+class Dropout(Layer):
+    def __init__(self, dropout_rate=0.5):
+        super().__init__()
+        self.dropout_rate = dropout_rate
+        self.mask = None
+
+    def forward(self, x):
+        """
+        Прямое распространение для Dropout(реализовал инвертированную версию - в ней масштабирование делается на train-е)
+
+        Args:
+            x: входной тензор
+
+        Returns:
+            выходной тензор с примененным dropout (в режиме обучения)
+        """
+        if self.training:
+            self.mask = (np.random.rand(*x.shape) > self.dropout_rate).astype(x.dtype)
+            output = x * self.mask / (1 - self.dropout_rate)
+        else:
+            output = x
+            self.mask = None
+
+        return output
+
+    def backward(self, grad_output):
+        """
+        Обратное распространение для Dropout
+
+        Args:
+            grad_output: градиент от следующего слоя
+
+        Returns:
+            градиент для предыдущего слоя
+        """
+        if self.training:
+            grad_input = grad_output * self.mask / (1 - self.dropout_rate)
+        else:
+            grad_input = grad_output
+
+        return grad_input
+
+
+class BatchNorm(Layer):
+    def __init__(self, num_features, eps=1e-5, momentum=0.1):
+        super().__init__()
+        self.num_features = num_features
+        self.eps = eps
+        self.momentum = momentum
+
+        self.gamma = np.ones(num_features)
+        self.beta = np.zeros(num_features)
+
+        self.running_mean = np.zeros(num_features)
+        self.running_var = np.ones(num_features)
+
+        # Переменные для backward pass
+        self.batch_mean = None
+        self.batch_var = None
+        self.normalized = None
+        self.input = None
+        self.grad_gamma = None
+        self.grad_beta = None
+
+    def forward(self, x):
+        """
+        Прямое распространение для Batch Normalization
+
+        Args:
+            x: входной тензор формы (batch_size, num_features)
+
+        Returns:
+            нормализованный выходной тензор той же формы
+        """
+        self.input = x
+
+        if self.training:
+            self.batch_mean = np.mean(x, axis=0)
+            self.batch_var = np.var(x, axis=0)
+
+            self.running_mean = (
+                1 - self.momentum
+            ) * self.running_mean + self.batch_mean * self.momentum
+            self.running_var = (
+                1 - self.momentum
+            ) * self.running_var + self.batch_var * self.momentum
+
+            mean = self.batch_mean
+            var = self.batch_var
+        else:
+            mean = self.running_mean
+            var = self.running_var
+
+        self.normalized = (x - mean) / np.sqrt(var + self.eps)
+
+        output = self.gamma * self.normalized + self.beta
+
+        return output
+
+    def backward(self, grad_output):
+        """
+        Обратное распространение для Batch Normalization
+        """
+        m = grad_output.shape[0]
+        self.grad_gamma = np.sum(grad_output * self.normalized, axis=0)
+        self.grad_beta = np.sum(grad_output, axis=0)
+
+        std_inv = 1.0 / np.sqrt(self.batch_var + self.eps)
+
+        dx_norm = grad_output * self.gamma
+
+        grad_input = (
+            1.0
+            / m
+            * std_inv
+            * (
+                m * dx_norm
+                - np.sum(dx_norm, axis=0)
+                - self.normalized * np.sum(dx_norm * self.normalized, axis=0)
+            )
+        )
+
+        return grad_input
+
+    def update_weights(self, learning_rate=0.01):
+        """
+        Обновление параметров
+        """
+        if self.grad_gamma is not None:
+            self.gamma -= learning_rate * self.grad_gamma
+
+        if self.grad_beta is not None:
+            self.beta -= learning_rate * self.grad_beta
+
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
